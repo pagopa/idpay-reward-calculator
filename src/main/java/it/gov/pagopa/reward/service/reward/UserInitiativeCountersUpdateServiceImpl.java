09040651e0fc9d28a1b5af3fa2c440ecb99495c0
pelliccm <matteo.pelliccione.sa@nttdata.com>
2022-07-26 11:25:47 +0200

    IREER-99 UserInitiativeCountersUpdateServiceImpl with periodic threshold update

--- src/main/java/it/gov/pagopa/reward/service/reward/UserInitiativeCountersUpdateServiceImpl.java
+++ src/main/java/it/gov/pagopa/reward/service/reward/UserInitiativeCountersUpdateServiceImpl.java
@@ -1,14 +1,21 @@
 package it.gov.pagopa.reward.service.reward;
 
+import it.gov.pagopa.reward.dto.InitiativeConfig;
 import it.gov.pagopa.reward.dto.RewardTransactionDTO;
+import it.gov.pagopa.reward.model.counters.Counters;
 import it.gov.pagopa.reward.model.counters.InitiativeCounters;
 import it.gov.pagopa.reward.model.counters.UserInitiativeCounters;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class UserInitiativeCountersUpdateServiceImpl implements UserInitiativeCountersUpdateService{
+
+    private final DroolsContainerHolderService droolsContainerHolderService;
+
+    public UserInitiativeCountersUpdateServiceImpl(DroolsContainerHolderService droolsContainerHolderService) {
+        this.droolsContainerHolderService = droolsContainerHolderService;
+    }
 
     @Override
     public void update(RewardTransactionDTO ruleEngineResult, UserInitiativeCounters userInitiativeCounters) {
@@ -17,11 +24,31 @@ public class UserInitiativeCountersUpdateServiceImpl implements UserInitiativeCo
         out.setInitiatives(userInitiativeCounters.getInitiatives());
 
         out.getInitiatives().forEach(o -> {
-            o.setTrxNumber(o.getTrxNumber() + 1);
-            o.setTotalReward(o.getTotalReward().add(ruleEngineResult.getRewards().get(o.getInitiativeId())));
-            o.setTotalAmount(ruleEngineResult.getAmount());
+            updateCounters(o, ruleEngineResult, o.getInitiativeId());
+            updateThresholdCounters(o, ruleEngineResult, droolsContainerHolderService.getInitiativeConfig(o.getInitiativeId()));
         });
+    }
 
-        // TODO update daily, monthly, yearly counters according to getInitiativeConfig boolean
+    private void updateThresholdCounters(InitiativeCounters initiativeCounters, RewardTransactionDTO ruleEngineResult, InitiativeConfig initiativeConfig) {
+
+        String dayFormattedDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String monthFormattedDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM"));
+        String yearFormattedDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy"));
+
+        if(initiativeConfig.isHasDailyThreshold()) {
+            updateCounters(initiativeCounters.getDailyCounters().get(dayFormattedDate), ruleEngineResult, initiativeCounters.getInitiativeId());
+        }
+        if(initiativeConfig.isHasMonthlyThreshold()) {
+            updateCounters(initiativeCounters.getMonthlyCounters().get(monthFormattedDate), ruleEngineResult, initiativeCounters.getInitiativeId());
+        }
+        if(initiativeConfig.isHasDailyThreshold()) {
+            updateCounters(initiativeCounters.getYearlyCounters().get(yearFormattedDate), ruleEngineResult, initiativeCounters.getInitiativeId());
+        }
+    }
+
+    private void updateCounters(Counters counters, RewardTransactionDTO ruleEngineResult, String initiativeId){
+        counters.setTrxNumber(counters.getTrxNumber() + 1);
+        counters.setTotalReward(counters.getTotalReward().add(ruleEngineResult.getRewards().get(initiativeId)));
+        counters.setTotalAmount(ruleEngineResult.getAmount());
     }
 }
