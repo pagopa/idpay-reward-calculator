e0e6431e437bb8937623e16778e0a72ec809adb8
anttorre <antonio.torre@everis.com>
2022-07-29 03:15:53 +0200

    fix codesmell

--- src/main/java/it/gov/pagopa/reward/service/reward/OnboardedInitiativesService.java
+++ src/main/java/it/gov/pagopa/reward/service/reward/OnboardedInitiativesService.java
@@ -3,7 +3,6 @@ package it.gov.pagopa.reward.service.reward;
 import reactor.core.publisher.Flux;
 
 import java.time.OffsetDateTime;
-import java.util.List;
 
 /**
  * This component will retrieve initiatives to which the input hpan has been enabled
