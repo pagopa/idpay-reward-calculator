d1c3b7861dc9b5037494106a6eea2fcafc1a003e
anttorre <antonio.torre@everis.com>
2022-09-13 19:07:01 +0200

    IREER-145 updating counters when refunds

--- src/main/java/it/gov/pagopa/reward/dto/trx/RefundInfo.java
+++ src/main/java/it/gov/pagopa/reward/dto/trx/RefundInfo.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.reward.dto.trx;
 
 import it.gov.pagopa.reward.model.TransactionProcessed;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.util.Map;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class RefundInfo {
     private List<TransactionProcessed> previousTrxs;
     private Map<String, BigDecimal> previousRewards;
