1ff8544dcb35dd860f19753245d5b506b47c991a
anttorre <antonio.torre@everis.com>
2022-09-15 13:36:43 +0200

    Enabling transaction processor binding when context ready

--- src/test/java/it/gov/pagopa/reward/BaseIntegrationTest.java
+++ src/test/java/it/gov/pagopa/reward/BaseIntegrationTest.java
@@ -62,6 +62,9 @@ import static org.awaitility.Awaitility.await;
 }, controlledShutdown = true)
 @TestPropertySource(
         properties = {
+                // even if enabled into application.yml, spring test will not load it https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.testing.spring-boot-applications.jmx
+                "spring.jmx.enabled=true",
+
                 //region common feature disabled
                 "app.reward-rule.cache.refresh-ms-rate=60000",
                 "logging.level.it.gov.pagopa.reward.service.ErrorNotifierServiceImpl=WARN",
@@ -126,10 +129,15 @@ public abstract class BaseIntegrationTest {
     public static void unregisterPreviouslyKafkaServers() throws MalformedObjectNameException, MBeanRegistrationException, InstanceNotFoundException {
         TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Rome")));
 
-        ObjectName kafkaServerMbeanName = new ObjectName("kafka.server:type=app-info,id=0");
+        unregisterMBean("kafka.server:type=app-info,id=0");
+        unregisterMBean("org.springframework.*:*");
+    }
+
+    private static void unregisterMBean(String objectName) throws MalformedObjectNameException, InstanceNotFoundException, MBeanRegistrationException {
+        ObjectName mbeanName = new ObjectName(objectName);
         MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
-        if (mBeanServer.isRegistered(kafkaServerMbeanName)) {
-            mBeanServer.unregisterMBean(kafkaServerMbeanName);
+        for (ObjectInstance mBean : mBeanServer.queryMBeans(mbeanName, null)) {
+            mBeanServer.unregisterMBean(mBean.getObjectName());
         }
     }
 
--- src/test/java/it/gov/pagopa/reward/service/reward/RewardContextHolderServiceImplTest.java
+++ src/test/java/it/gov/pagopa/reward/service/reward/RewardContextHolderServiceImplTest.java
@@ -6,94 +6,79 @@ import it.gov.pagopa.reward.repository.DroolsRuleRepository;
 import it.gov.pagopa.reward.service.build.KieContainerBuilderService;
 import it.gov.pagopa.reward.test.utils.TestUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.kie.api.runtime.KieContainer;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@ExtendWith(MockitoExtension.class)
 class RewardContextHolderServiceImplTest {
+
+    @Mock private KieContainerBuilderService kieContainerBuilderServiceMock;
+    @Mock private DroolsRuleRepository droolsRuleRepositoryMock;
+    @Mock private ApplicationEventPublisher applicationEventPublisherMock;
+
+    private RewardContextHolderService rewardContextHolderService;
+
+    private final KieContainer expectedKieContainer = Mockito.mock(KieContainer.class);
+
+    @BeforeEach
+    void init(){
+        Mockito.when(droolsRuleRepositoryMock.findAll()).thenReturn(Flux.empty());
+        Mockito.when(kieContainerBuilderServiceMock.build(Mockito.any())).thenReturn(Mono.just(expectedKieContainer));
+
+        rewardContextHolderService = new RewardContextHolderServiceImpl(kieContainerBuilderServiceMock, droolsRuleRepositoryMock, applicationEventPublisherMock);
+    }
 
     @Test
     void getKieContainer() {
-        // Given
-        KieContainerBuilderService kieContainerBuilderService = Mockito.mock(KieContainerBuilderService.class);
-        DroolsRuleRepository droolsRuleRepository = Mockito.mock(DroolsRuleRepository.class);
-        KieContainer kieContainer = Mockito.mock(KieContainer.class);
-
-        Mockito.when(droolsRuleRepository.findAll()).thenReturn(Flux.empty());
-        Mockito.when(kieContainerBuilderService.build(Mockito.any())).thenReturn(Mono.just(kieContainer));
-
-        RewardContextHolderService rewardContextHolderService = new RewardContextHolderServiceImpl(kieContainerBuilderService, droolsRuleRepository);
-
         // When
         KieContainer result = rewardContextHolderService.getRewardRulesKieContainer();
 
         //Then
         Assertions.assertNotNull(result);
-        Assertions.assertEquals(kieContainer, result);
+        Assertions.assertSame(expectedKieContainer, result);
     }
 
     @Test
     void testNotRetrieveInitiativeConfig(){
-        KieContainerBuilderService kieContainerBuilderService = Mockito.mock(KieContainerBuilderService.class);
-        DroolsRuleRepository droolsRuleRepository = Mockito.mock(DroolsRuleRepository.class);
-        KieContainer kieContainer = Mockito.mock(KieContainer.class);
-
-        Mockito.when(droolsRuleRepository.findAll()).thenReturn(Flux.empty());
-        Mockito.when(kieContainerBuilderService.build(Mockito.any())).thenReturn(Mono.just(kieContainer));
-
-        RewardContextHolderService rewardContextHolderService = new RewardContextHolderServiceImpl(kieContainerBuilderService, droolsRuleRepository);
-
         String initiativeId="INITIATIVE-ID";
-        Mockito.when(droolsRuleRepository.findById(Mockito.same(initiativeId))).thenReturn(Mono.empty());
+        Mockito.when(droolsRuleRepositoryMock.findById(Mockito.same(initiativeId))).thenReturn(Mono.empty());
 
         // When
         InitiativeConfig result = rewardContextHolderService.getInitiativeConfig(initiativeId);
 
         //Then
         Assertions.assertNull(result);
-        Mockito.verify(droolsRuleRepository).findById(Mockito.same(initiativeId));
+        Mockito.verify(droolsRuleRepositoryMock).findById(Mockito.same(initiativeId));
     }
 
     @Test
     void testRetrieveInitiativeConfig(){
-        KieContainerBuilderService kieContainerBuilderService = Mockito.mock(KieContainerBuilderService.class);
-        DroolsRuleRepository droolsRuleRepository = Mockito.mock(DroolsRuleRepository.class);
-        KieContainer kieContainer = Mockito.mock(KieContainer.class);
-
-        Mockito.when(droolsRuleRepository.findAll()).thenReturn(Flux.empty());
-        Mockito.when(kieContainerBuilderService.build(Mockito.any())).thenReturn(Mono.just(kieContainer));
-
-        RewardContextHolderService rewardContextHolderService = new RewardContextHolderServiceImpl(kieContainerBuilderService, droolsRuleRepository);
-
         String initiativeId="INITIATIVE-ID";
         InitiativeConfig initiativeConfig = Mockito.mock(InitiativeConfig.class);
         DroolsRule droolsRule = DroolsRule.builder().initiativeConfig(initiativeConfig).build();
-        Mockito.when(droolsRuleRepository.findById(Mockito.same(initiativeId))).thenReturn(Mono.just(droolsRule));
+        Mockito.when(droolsRuleRepositoryMock.findById(Mockito.same(initiativeId))).thenReturn(Mono.just(droolsRule));
 
         // When
         InitiativeConfig result = rewardContextHolderService.getInitiativeConfig(initiativeId);
 
         //Then
         Assertions.assertNotNull(result);
-        Mockito.verify(droolsRuleRepository).findById(Mockito.same(initiativeId));
+        Mockito.verify(droolsRuleRepositoryMock).findById(Mockito.same(initiativeId));
     }
 
     @Test
     void testSetInitiativeConfig(){
-        KieContainerBuilderService kieContainerBuilderService = Mockito.mock(KieContainerBuilderService.class);
-        DroolsRuleRepository droolsRuleRepository = Mockito.mock(DroolsRuleRepository.class);
-        KieContainer kieContainer = Mockito.mock(KieContainer.class);
-
-        Mockito.when(droolsRuleRepository.findAll()).thenReturn(Flux.empty());
-        Mockito.when(kieContainerBuilderService.build(Mockito.any())).thenReturn(Mono.just(kieContainer));
-
-        RewardContextHolderService rewardContextHolderService = new RewardContextHolderServiceImpl(kieContainerBuilderService, droolsRuleRepository);
-
         String initiativeId="INITIATIVE-ID";
         InitiativeConfig initiativeConfig = InitiativeConfig.builder()
                 .initiativeId(initiativeId)
