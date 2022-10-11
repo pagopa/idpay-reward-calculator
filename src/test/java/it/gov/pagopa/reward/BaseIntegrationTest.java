53082be9787045423f1950a6e8f87ff1eeb9443e
anttorre <antonio.torre@nttdata.com>
2022-10-11 20:12:21 +0200

    fixed commit retry check

--- src/test/java/it/gov/pagopa/reward/BaseIntegrationTest.java
+++ src/test/java/it/gov/pagopa/reward/BaseIntegrationTest.java
@@ -296,8 +296,8 @@ public abstract class BaseIntegrationTest {
                 final Map<TopicPartition, OffsetAndMetadata> commits = getCommittedOffsets(topic, groupId);
                 Assertions.assertEquals(expectedCommittedMessages, commits.values().stream().mapToLong(OffsetAndMetadata::offset).sum());
                 return commits;
-            } catch (RuntimeException e){
-                lastException = e;
+            } catch (Throwable e){
+                lastException = new RuntimeException(e);
                 wait(millisAttemptDelay, TimeUnit.MILLISECONDS);
             }
         }
