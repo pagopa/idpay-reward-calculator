1603a9703a1d47a77e67b950b7ec1b9f1b91ce6d
anttorre <antonio.torre@nttdata.com>
2023-01-31 19:25:36 +0100

    fixed Kie build logic invocation during publisher creation

--- src/main/java/it/gov/pagopa/reward/service/build/KieContainerBuilderServiceImpl.java
+++ src/main/java/it/gov/pagopa/reward/service/build/KieContainerBuilderServiceImpl.java
@@ -12,7 +12,6 @@ import org.kie.api.builder.KieModule;
 import org.kie.api.builder.Message;
 import org.kie.api.definition.KiePackage;
 import org.kie.api.definition.rule.Rule;
-import org.kie.api.runtime.KieContainer;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -38,34 +37,33 @@ public class KieContainerBuilderServiceImpl implements KieContainerBuilderServic
 
     @Override
     public Mono<KieBase> build(Flux<DroolsRule> rules) {
-        KieServices kieServices = KieServices.Factory.get();
-        KieFileSystem kieFileSystem = KieServices.get().newKieFileSystem();
+        return Mono.defer(() -> {
+            KieServices kieServices = KieServices.Factory.get();
+            KieFileSystem kieFileSystem = KieServices.get().newKieFileSystem();
 
-        return rules.map(r -> kieFileSystem.write(String.format("src/main/resources/%s/%s.drl", RULES_BUILT_DIR, r.getName()), r.getRule()))
-                .then(Mono.fromSupplier(() -> {
-                    KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
-                    kieBuilder.buildAll();
+            return rules.map(r -> kieFileSystem.write(String.format("src/main/resources/%s/%s.drl", RULES_BUILT_DIR, r.getName()), r.getRule()))
+                    .then(Mono.fromSupplier(() -> {
+                        KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
+                        kieBuilder.buildAll();
 
-                    if (kieBuilder.getResults().hasMessages(Message.Level.ERROR)) {
-                        /* TODO if a stored rule don't compile, stop the container build or ignore the rule?
-                        kieBuilder.getResults().getMessages(Message.Level.ERROR).stream().map(Message::getPath).forEach(kieFileSystem::delete);
-                        */
-                        throw new IllegalArgumentException("Build Errors:" + kieBuilder.getResults().toString());
-                    }
+                        if (kieBuilder.getResults().hasMessages(Message.Level.ERROR)) {
+                            throw new IllegalArgumentException("Build Errors:" + kieBuilder.getResults().toString());
+                        }
 
-                    KieModule kieModule = kieBuilder.getKieModule();
-                    KieBase newKieBase = kieServices.newKieContainer(kieModule.getReleaseId()).getKieBase();
+                        KieModule kieModule = kieBuilder.getKieModule();
+                        KieBase newKieBase = kieServices.newKieContainer(kieModule.getReleaseId()).getKieBase();
 
-                    log.info("Build completed");
-                    if (log.isDebugEnabled()) {
-                        KiePackage kiePackage = newKieBase.getKiePackage(RULES_BUILT_PACKAGE);
-                        log.debug("The container now will contain the following rules inside %s package: %s".formatted(
-                                RULES_BUILT_PACKAGE,
-                                kiePackage != null
-                                        ? kiePackage.getRules().stream().map(Rule::getId).toList()
-                                        : "0"));
-                    }
-                    return newKieBase;
-                }));
+                        log.info("Build completed");
+                        if (log.isDebugEnabled()) {
+                            KiePackage kiePackage = newKieBase.getKiePackage(RULES_BUILT_PACKAGE);
+                            log.debug("The container now will contain the following rules inside %s package: %s".formatted(
+                                    RULES_BUILT_PACKAGE,
+                                    kiePackage != null
+                                            ? kiePackage.getRules().stream().map(Rule::getId).toList()
+                                            : "0"));
+                        }
+                        return newKieBase;
+                    }));
+        });
     }
 }
