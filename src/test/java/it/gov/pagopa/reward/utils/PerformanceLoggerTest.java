ab8d1df8b0d44958cf0cd556aa7ff1fc56541967
anttorre <antonio.torre@nttdata.com>
2023-02-10 13:22:27 +0100

    PerformanceLogger test complete

--- src/test/java/it/gov/pagopa/reward/utils/PerformanceLoggerTest.java
+++ src/test/java/it/gov/pagopa/reward/utils/PerformanceLoggerTest.java
@@ -5,13 +5,20 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.function.Function;
+
 class PerformanceLoggerTest {
 
 //region mono ops
     @Test
     void testMonoLogTimingOnNext(){
+        testMonoLogTimingOnNext(null);
+        testMonoLogTimingOnNext(Object::toString);
+    }
+
+    private static void testMonoLogTimingOnNext(Function<Boolean, String> data2LogPayload) {
         boolean[] result=new boolean[]{false};
-        Mono<Boolean> mono = PerformanceLogger.logTimingOnNext("PROVA", Mono.defer(() -> Mono.just(result[0] = true)), null);
+        Mono<Boolean> mono = PerformanceLogger.logTimingOnNext("PROVA", Mono.defer(() -> Mono.just(result[0] = true)), data2LogPayload);
         Assertions.assertFalse(result[0]);
         mono.block();
         Assertions.assertTrue(result[0]);
@@ -19,8 +26,13 @@ class PerformanceLoggerTest {
 
     @Test
     void testMonoLogTimingFinally(){
+        testMonoLogTimingFinally(null);
+        testMonoLogTimingFinally("END");
+    }
+
+    private static void testMonoLogTimingFinally(String logPayload) {
         boolean[] result=new boolean[]{false};
-        Mono<Boolean> mono = PerformanceLogger.logTimingFinally("PROVA", Mono.defer(() -> Mono.just(result[0] = true)), "END");
+        Mono<Boolean> mono = PerformanceLogger.logTimingFinally("PROVA", Mono.defer(() -> Mono.just(result[0] = true)), logPayload);
         Assertions.assertFalse(result[0]);
         mono.block();
         Assertions.assertTrue(result[0]);
@@ -29,9 +41,14 @@ class PerformanceLoggerTest {
 
     //region flux ops
     @Test
-    void testFlux(){
+    void testFluxLogTimingOnNext(){
+        testFluxLogTimingOnNext(null);
+        testFluxLogTimingOnNext(Object::toString);
+    }
+
+    private static void testFluxLogTimingOnNext(Function<Boolean, String> data2LogPayload) {
         boolean[] result=new boolean[]{false};
-        Flux<Boolean> flux = PerformanceLogger.logTimingOnNext("PROVA", Flux.defer(() -> Flux.just(result[0] = true)), null);
+        Flux<Boolean> flux = PerformanceLogger.logTimingOnNext("PROVA", Flux.defer(() -> Flux.just(result[0] = true)), data2LogPayload);
         Assertions.assertFalse(result[0]);
         flux.collectList().block();
         Assertions.assertTrue(result[0]);
@@ -39,8 +56,13 @@ class PerformanceLoggerTest {
 
     @Test
     void testFluxLogTimingFinally(){
+        testFluxLogTimingFinally(null);
+        testFluxLogTimingFinally("END");
+    }
+
+    private static void testFluxLogTimingFinally(String logPayload) {
         boolean[] result=new boolean[]{false};
-        Flux<Boolean> flux = PerformanceLogger.logTimingFinally("PROVA", Flux.defer(() -> Flux.just(result[0] = true)), "END");
+        Flux<Boolean> flux = PerformanceLogger.logTimingFinally("PROVA", Flux.defer(() -> Flux.just(result[0] = true)), logPayload);
         Assertions.assertFalse(result[0]);
         flux.collectList().block();
         Assertions.assertTrue(result[0]);
