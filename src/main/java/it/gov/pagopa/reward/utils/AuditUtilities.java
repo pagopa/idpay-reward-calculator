b28ce23465babc05b5aeff122931d3a034b81539
anttorre <antonio.torre@nttdata.com>
2023-02-21 18:44:52 +0100

    refactor auditLog fixed reward amount print fixed unitTests

--- src/main/java/it/gov/pagopa/reward/utils/AuditUtilities.java
+++ src/main/java/it/gov/pagopa/reward/utils/AuditUtilities.java
@@ -3,8 +3,6 @@ package it.gov.pagopa.reward.utils;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Map;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import it.gov.pagopa.reward.dto.trx.RewardTransactionDTO;
@@ -16,46 +14,54 @@ import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
-@Slf4j
+@Slf4j(topic = "AUDIT")
 public class AuditUtilities {
-    private static String SRCIP;
+    public static final String SRCIP;
 
     static {
+        String srcIp;
         try {
-            SRCIP = InetAddress.getLocalHost().getHostAddress();
+            srcIp = InetAddress.getLocalHost().getHostAddress();
         } catch (UnknownHostException e) {
-            log.error(e.getMessage());
+            log.error("Cannot determine the ip of the current host", e);
+            srcIp="UNKNOWN";
         }
+
+        SRCIP = srcIp;
     }
 
     private static final String CEF = String.format("CEF:0|PagoPa|IDPAY|1.0|7|User interaction|2| event=Reward dstip=%s", SRCIP);
-    private static final String MSG = " msg=";
-    private static final String USER = "suser=";
-    private static final String TRX_ISSUER = "cs1Label=TRXIssuer cs1=";
-    private static final String TRX_ACQUIRER = "cs2Label=TRXAcquirer cs2=";
-    private static final String REWARDS = "cs3Label=rewards cs3=";
-    private static final String CORRELATION_ID = "cs4Label=correlationId cs4=";
-    final Logger logger = Logger.getLogger("AUDIT");
 
+    private void logAuditString(String userId) {
+        log.info(userId);
+    }
 
-    private String buildLog(String eventLog, String userId, String trxIssuer, String trxAcquirer, String rewards) {
-        return CEF + MSG + eventLog + " " + USER + userId + " " + TRX_ISSUER + trxIssuer + " " + TRX_ACQUIRER + trxAcquirer + " " + REWARDS + rewards;
+    private String buildBaseAuditLog(String eventLog, String userId, String trxIssuer, String trxAcquirer, String rewards) {
+        return "%s msg=%s suser=%s cs1Label=TRXIssuer cs1=%s cs2Label=TRXAcquirer cs2=%s cs3Label=rewards cs3=%s".formatted(
+                CEF, eventLog, userId, trxIssuer, trxAcquirer, rewards);
     }
 
     public void logCharge(String userId, String trxIssuer, String trxAcquirer, String rewards) {
-        String testLog = this.buildLog("The charge has been calculated", userId, trxIssuer, trxAcquirer, rewards);
-        logger.info(testLog);
+        logAuditString(
+                this.buildBaseAuditLog("The charge has been calculated", userId, trxIssuer, trxAcquirer, rewards)
+        );
     }
 
     public void logRefund(String userId, String trxIssuer, String trxAcquirer, String rewards, String correlationId) {
-        String testLog = this.buildLog("The refund has been calculated", userId, trxIssuer, trxAcquirer, rewards) + " " + CORRELATION_ID + correlationId;
-        logger.info(testLog);
+        logAuditString(
+                "%s cs4Label=correlationId cs4=%s".formatted(
+                this.buildBaseAuditLog("The refund has been calculated", userId, trxIssuer, trxAcquirer, rewards),
+                correlationId)
+        );
     }
 
-    public void logExecute(TransactionDTO payload, RewardTransactionDTO r) {
-        String rewards = r.getRewards().entrySet().stream().collect(Collectors.toMap(entry ->
-                "reward=".concat(entry.getValue().getProvidedReward().toString())
-                        .concat(" initiativeId"), Map.Entry::getKey)).toString();
+    public void logExecute(TransactionDTO payload, RewardTransactionDTO trx) {
+        String rewards = trx.getRewards().values().stream()
+                .map(r -> "rewardCents=%s initiativeId=%s".formatted(
+                        Utils.euroToCents(r.getAccruedReward()), r.getInitiativeId()
+                ))
+                .collect(Collectors.joining(" "));
+
         if (OperationType.CHARGE.equals(payload.getOperationTypeTranscoded())) {
             this.logCharge(payload.getUserId(), payload.getIdTrxIssuer(), payload.getIdTrxAcquirer(), rewards);
         } else if (OperationType.REFUND.equals(payload.getOperationTypeTranscoded())) {
