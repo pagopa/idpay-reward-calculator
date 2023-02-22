78fc81344feb353625a3ea89058f3d36d3e5ea2d
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2023-02-22 12:57:29 +0100

    refactor auditLog (#101)
    * refactor auditLog
    fixed reward amount print
    fixed unitTests
    
    * letting log appender format message

--- src/test/java/it/gov/pagopa/reward/utils/AuditUtilitiesTest.java
+++ src/test/java/it/gov/pagopa/reward/utils/AuditUtilitiesTest.java
@@ -1,43 +1,22 @@
 package it.gov.pagopa.reward.utils;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.read.ListAppender;
-import java.net.InetAddress;
-import java.util.Collections;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
+import it.gov.pagopa.reward.test.utils.MemoryAppender;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith({SpringExtension.class, MockitoExtension.class})
-@ContextConfiguration(classes = {AuditUtilities.class,InetAddress.class})
 class AuditUtilitiesTest {
-    private static final String MSG = " TEST_MSG";
     private static final String USER = "TEST_USER_ID";
     private static final String CORRELATION_ID = "TEST_CORRELATION_ID";
     private static final String TRX_ISSUER = "TEST_TRX_ISSUER";
     private static final String TRX_ACQUIRER = "TEST_TRX_ACQUIRER";
     private static final String REWARD = "TEST_REWARD";
 
-    @MockBean
-    Logger logger;
-    @Autowired
-    AuditUtilities auditUtilities;
-    @MockBean
-    InetAddress inetAddress;
-    MemoryAppender memoryAppender;
+    private MemoryAppender memoryAppender;
+
+    private final AuditUtilities auditUtilities = new AuditUtilities();
 
     @BeforeEach
     public void setup() {
@@ -49,57 +28,53 @@ class AuditUtilitiesTest {
         memoryAppender.start();
     }
 
-
     @Test
     void logCharge_ok(){
         auditUtilities.logCharge(USER, TRX_ISSUER, TRX_ACQUIRER, REWARD);
-        assertThat(memoryAppender.contains(ch.qos.logback.classic.Level.DEBUG,MSG)).isFalse();
+
+        checkCommonFields();
+
+        Assertions.assertEquals(
+                "CEF:0|PagoPa|IDPAY|1.0|7|User interaction|2| event=Reward dstip=%s msg=The charge has been calculated suser=%s cs1Label=TRXIssuer cs1=%s cs2Label=TRXAcquirer cs2=%s cs3Label=rewards cs3=%s"
+                        .formatted(
+                                AuditUtilities.SRCIP,
+                                USER,
+                                TRX_ISSUER,
+                                TRX_ACQUIRER,
+                                REWARD
+                        ),
+                memoryAppender.getLoggedEvents().get(0).getFormattedMessage()
+        );
+    }
+
+    private void checkCommonFields() {
+        Assertions.assertTrue(memoryAppender.contains(ch.qos.logback.classic.Level.INFO,USER));
+        Assertions.assertTrue(memoryAppender.contains(ch.qos.logback.classic.Level.INFO,TRX_ISSUER));
+        Assertions.assertTrue(memoryAppender.contains(ch.qos.logback.classic.Level.INFO,TRX_ACQUIRER));
+        Assertions.assertTrue(memoryAppender.contains(ch.qos.logback.classic.Level.INFO,REWARD));
+
+        Assertions.assertEquals(1, memoryAppender.getLoggedEvents().size());
     }
 
     @Test
     void logRefund_ok(){
         auditUtilities.logRefund(USER, TRX_ISSUER, TRX_ACQUIRER, REWARD, CORRELATION_ID);
-        assertThat(memoryAppender.contains(ch.qos.logback.classic.Level.DEBUG,MSG)).isFalse();
-    }
 
+        checkCommonFields();
+        Assertions.assertTrue(memoryAppender.contains(ch.qos.logback.classic.Level.INFO,CORRELATION_ID));
 
-    public static class MemoryAppender extends ListAppender<ILoggingEvent> {
-        public void reset() {
-            this.list.clear();
-        }
-
-        public boolean contains(ch.qos.logback.classic.Level level, String string) {
-            return this.list.stream()
-                    .anyMatch(event -> event.toString().contains(string)
-                            && event.getLevel().equals(level));
-        }
-
-        public int countEventsForLogger(String loggerName) {
-            return (int) this.list.stream()
-                    .filter(event -> event.getLoggerName().contains(loggerName))
-                    .count();
-        }
-
-        public List<ILoggingEvent> search() {
-            return this.list.stream()
-                    .filter(event -> event.toString().contains(MSG))
-                    .collect(Collectors.toList());
-        }
-
-        public List<ILoggingEvent> search(Level level) {
-            return this.list.stream()
-                    .filter(event -> event.toString().contains(MSG)
-                            && event.getLevel().equals(level))
-                    .collect(Collectors.toList());
-        }
-
-        public int getSize() {
-            return this.list.size();
-        }
-
-        public List<ILoggingEvent> getLoggedEvents() {
-            return Collections.unmodifiableList(this.list);
-        }
+        Assertions.assertEquals(
+                "CEF:0|PagoPa|IDPAY|1.0|7|User interaction|2| event=Reward dstip=%s msg=The refund has been calculated suser=%s cs1Label=TRXIssuer cs1=%s cs2Label=TRXAcquirer cs2=%s cs3Label=rewards cs3=%s cs4Label=correlationId cs4=%s"
+                        .formatted(
+                                AuditUtilities.SRCIP,
+                                USER,
+                                TRX_ISSUER,
+                                TRX_ACQUIRER,
+                                REWARD,
+                                CORRELATION_ID
+                                ),
+                memoryAppender.getLoggedEvents().get(0).getFormattedMessage()
+        );
     }
 
 }
