1acf0eec595b5dcc8fa7930f665b3d757de08592
anttorre <antonio.torre@nttdata.com>
2023-03-09 17:04:33 +0100

    Added rejection reasons to audit logs

--- src/main/java/it/gov/pagopa/reward/utils/AuditUtilities.java
+++ src/main/java/it/gov/pagopa/reward/utils/AuditUtilities.java
@@ -1,16 +1,15 @@
 package it.gov.pagopa.reward.utils;
 
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.stream.Collectors;
-
 import it.gov.pagopa.reward.dto.trx.RewardTransactionDTO;
-import it.gov.pagopa.reward.dto.trx.TransactionDTO;
 import it.gov.pagopa.reward.enums.OperationType;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.stream.Collectors;
 
 @Component
 @AllArgsConstructor
@@ -31,38 +30,45 @@ public class AuditUtilities {
     }
 
     private static final String CEF = String.format("CEF:0|PagoPa|IDPAY|1.0|7|User interaction|2| event=Reward dstip=%s", SRCIP);
-    private static final String CEF_PATTERN = CEF + " msg={} suser={} cs1Label=TRXIssuer cs1={} cs2Label=TRXAcquirer cs2={} cs3Label=rewards cs3={}";
-    private static final String CEF_CORRELATED_PATTERN = CEF_PATTERN + " cs4Label=correlationId cs4={}";
+    private static final String CEF_PATTERN = CEF + " msg={} suser={} cs1Label=TRXIssuer cs1={} cs2Label=TRXAcquirer cs2={} cs3Label=rewards cs3={} cs4Label=rejectionReasons cs4={} cs5Label=initiativeRejectionReasons cs5={}";
+    private static final String CEF_CORRELATED_PATTERN = CEF_PATTERN + " cs6Label=correlationId cs6={}";
 
     private void logAuditString(String pattern, String... parameters) {
         log.info(pattern, (Object[]) parameters);
     }
 
-    public void logCharge(String userId, String trxIssuer, String trxAcquirer, String rewards) {
+    public void logCharge(String userId, String trxIssuer, String trxAcquirer, String rewards, String rejectionReasons, String initiativeRejectionReasons) {
         logAuditString(
                 CEF_PATTERN,
-                "The charge has been calculated", userId, trxIssuer, trxAcquirer, rewards
+                "The charge has been calculated", userId, trxIssuer, trxAcquirer, rewards, rejectionReasons, initiativeRejectionReasons
         );
     }
 
-    public void logRefund(String userId, String trxIssuer, String trxAcquirer, String rewards, String correlationId) {
+    public void logRefund(String userId, String trxIssuer, String trxAcquirer, String rewards, String rejectionReasons, String initiativeRejectionReasons, String correlationId) {
         logAuditString(
                 CEF_CORRELATED_PATTERN,
-                "The refund has been calculated", userId, trxIssuer, trxAcquirer, rewards, correlationId
+                "The refund has been calculated", userId, trxIssuer, trxAcquirer, rewards, rejectionReasons, initiativeRejectionReasons, correlationId
         );
     }
 
-    public void logExecute(TransactionDTO payload, RewardTransactionDTO trx) {
-        String rewards = trx.getRewards().values().stream()
-                .map(r -> "rewardCents=%s initiativeId=%s".formatted(
-                        Utils.euroToCents(r.getAccruedReward()), r.getInitiativeId()
+    public void logExecute(RewardTransactionDTO trx) {
+        String rewards = "["+trx.getRewards().values().stream()
+                .map(r -> "initiativeId=%s rewardCents=%s".formatted(
+                        r.getInitiativeId(), Utils.euroToCents(r.getAccruedReward())
                 ))
-                .collect(Collectors.joining(" "));
+                .collect(Collectors.joining(","))+"]";
 
-        if (OperationType.CHARGE.equals(payload.getOperationTypeTranscoded())) {
-            this.logCharge(payload.getUserId(), payload.getIdTrxIssuer(), payload.getIdTrxAcquirer(), rewards);
-        } else if (OperationType.REFUND.equals(payload.getOperationTypeTranscoded())) {
-            this.logRefund(payload.getUserId(), payload.getIdTrxIssuer(), payload.getIdTrxAcquirer(), rewards, payload.getCorrelationId());
+        String rejectionReasons = "[" + String.join(",", trx.getRejectionReasons()) + "]";
+        String initiativeRejectionReasons = "[" +
+                trx.getInitiativeRejectionReasons().entrySet().stream()
+                        .map(e -> e.getKey() + "=[" + String.join(",", e.getValue()) + "]")
+                        .collect(Collectors.joining(","))
+                + "]";
+
+        if (OperationType.CHARGE.equals(trx.getOperationTypeTranscoded())) {
+            this.logCharge(trx.getUserId(), trx.getIdTrxIssuer(), trx.getIdTrxAcquirer(), rewards, rejectionReasons, initiativeRejectionReasons);
+        } else if (OperationType.REFUND.equals(trx.getOperationTypeTranscoded())) {
+            this.logRefund(trx.getUserId(), trx.getIdTrxIssuer(), trx.getIdTrxAcquirer(), rewards, rejectionReasons, initiativeRejectionReasons, trx.getCorrelationId());
         }
     }
 }
--- src/test/java/it/gov/pagopa/reward/utils/AuditUtilitiesTest.java
+++ src/test/java/it/gov/pagopa/reward/utils/AuditUtilitiesTest.java
@@ -1,11 +1,20 @@
 package it.gov.pagopa.reward.utils;
 
 import ch.qos.logback.classic.LoggerContext;
+import it.gov.pagopa.reward.dto.mapper.TransactionDroolsDTO2RewardTransactionMapper;
+import it.gov.pagopa.reward.dto.trx.Reward;
+import it.gov.pagopa.reward.dto.trx.RewardTransactionDTO;
+import it.gov.pagopa.reward.enums.OperationType;
+import it.gov.pagopa.reward.test.fakers.TransactionDroolsDtoFaker;
 import it.gov.pagopa.reward.test.utils.MemoryAppender;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
 
 class AuditUtilitiesTest {
     private static final String USER = "TEST_USER_ID";
@@ -13,10 +22,13 @@ class AuditUtilitiesTest {
     private static final String TRX_ISSUER = "TEST_TRX_ISSUER";
     private static final String TRX_ACQUIRER = "TEST_TRX_ACQUIRER";
     private static final String REWARD = "TEST_REWARD";
+    private static final String REJECTIONREASONS = "TEST_REJECTIONREASONS";
+    private static final String INIATIATIVEREJECTIONREASONS = "TEST_INIATIATIVEREJECTIONREASONS";
 
     private MemoryAppender memoryAppender;
 
     private final AuditUtilities auditUtilities = new AuditUtilities();
+    private final TransactionDroolsDTO2RewardTransactionMapper mapper = new TransactionDroolsDTO2RewardTransactionMapper();
 
     @BeforeEach
     public void setup() {
@@ -29,19 +41,68 @@ class AuditUtilitiesTest {
     }
 
     @Test
+    void testLogExecute_chargeOp(){
+        // Given
+        RewardTransactionDTO trx = buildTrx(0);
+
+        // When
+        auditUtilities.logExecute(trx);
+
+        //Then
+        Assertions.assertEquals(
+                "CEF:0|PagoPa|IDPAY|1.0|7|User interaction|2| event=Reward dstip=%s msg=The charge has been calculated suser=USERID0 cs1Label=TRXIssuer cs1=IDTRXISSUER0 cs2Label=TRXAcquirer cs2=IDTRXACQUIRER0 cs3Label=rewards cs3=[initiativeId=INITIATIVE3 rewardCents=100] cs4Label=rejectionReasons cs4=[REJECTIONREASON1] cs5Label=initiativeRejectionReasons cs5=[INITIATIVE1=[REJECTIONREASON2],INITIATIVE2=[REJECTIONREASON3,REJECTIONREASON4]]"
+                        .formatted(AuditUtilities.SRCIP),
+                memoryAppender.getLoggedEvents().get(0).getFormattedMessage()
+        );
+    }
+
+    @Test
+    void testLogExecute_refundOp(){
+        // Given
+        RewardTransactionDTO trx = buildTrx(1);
+        trx.setOperationTypeTranscoded(OperationType.REFUND);
+        trx.setOperationType("01");
+
+        // When
+        auditUtilities.logExecute(trx);
+
+        //Then
+        Assertions.assertEquals(
+                "CEF:0|PagoPa|IDPAY|1.0|7|User interaction|2| event=Reward dstip=%s msg=The refund has been calculated suser=USERID1 cs1Label=TRXIssuer cs1=IDTRXISSUER1 cs2Label=TRXAcquirer cs2=IDTRXACQUIRER1 cs3Label=rewards cs3=[initiativeId=INITIATIVE3 rewardCents=100] cs4Label=rejectionReasons cs4=[REJECTIONREASON1] cs5Label=initiativeRejectionReasons cs5=[INITIATIVE1=[REJECTIONREASON2],INITIATIVE2=[REJECTIONREASON3,REJECTIONREASON4]] cs6Label=correlationId cs6=CORRELATIONID1"
+                        .formatted(AuditUtilities.SRCIP),
+                memoryAppender.getLoggedEvents().get(0).getFormattedMessage()
+        );
+    }
+
+    private RewardTransactionDTO buildTrx(int bias) {
+        RewardTransactionDTO trx = mapper.apply(TransactionDroolsDtoFaker.mockInstance(bias));
+        trx.setRejectionReasons(List.of("REJECTIONREASON1"));
+        trx.setInitiativeRejectionReasons(Map.of(
+                "INITIATIVE1", List.of("REJECTIONREASON2"),
+                "INITIATIVE2", List.of("REJECTIONREASON3", "REJECTIONREASON4")
+        ));
+        trx.setRewards(Map.of(
+                "INITIATIVE3", new Reward("INITIATIVE3", "ORGID", BigDecimal.ONE)
+        ));
+        return trx;
+    }
+
+    @Test
     void logCharge_ok(){
-        auditUtilities.logCharge(USER, TRX_ISSUER, TRX_ACQUIRER, REWARD);
+        auditUtilities.logCharge(USER, TRX_ISSUER, TRX_ACQUIRER, REWARD, REJECTIONREASONS, INIATIATIVEREJECTIONREASONS);
 
         checkCommonFields();
 
         Assertions.assertEquals(
-                "CEF:0|PagoPa|IDPAY|1.0|7|User interaction|2| event=Reward dstip=%s msg=The charge has been calculated suser=%s cs1Label=TRXIssuer cs1=%s cs2Label=TRXAcquirer cs2=%s cs3Label=rewards cs3=%s"
+                "CEF:0|PagoPa|IDPAY|1.0|7|User interaction|2| event=Reward dstip=%s msg=The charge has been calculated suser=%s cs1Label=TRXIssuer cs1=%s cs2Label=TRXAcquirer cs2=%s cs3Label=rewards cs3=%s cs4Label=rejectionReasons cs4=%s cs5Label=initiativeRejectionReasons cs5=%s"
                         .formatted(
                                 AuditUtilities.SRCIP,
                                 USER,
                                 TRX_ISSUER,
                                 TRX_ACQUIRER,
-                                REWARD
+                                REWARD,
+                                REJECTIONREASONS,
+                                INIATIATIVEREJECTIONREASONS
                         ),
                 memoryAppender.getLoggedEvents().get(0).getFormattedMessage()
         );
@@ -58,19 +119,21 @@ class AuditUtilitiesTest {
 
     @Test
     void logRefund_ok(){
-        auditUtilities.logRefund(USER, TRX_ISSUER, TRX_ACQUIRER, REWARD, CORRELATION_ID);
+        auditUtilities.logRefund(USER, TRX_ISSUER, TRX_ACQUIRER, REWARD, REJECTIONREASONS, INIATIATIVEREJECTIONREASONS, CORRELATION_ID);
 
         checkCommonFields();
         Assertions.assertTrue(memoryAppender.contains(ch.qos.logback.classic.Level.INFO,CORRELATION_ID));
 
         Assertions.assertEquals(
-                "CEF:0|PagoPa|IDPAY|1.0|7|User interaction|2| event=Reward dstip=%s msg=The refund has been calculated suser=%s cs1Label=TRXIssuer cs1=%s cs2Label=TRXAcquirer cs2=%s cs3Label=rewards cs3=%s cs4Label=correlationId cs4=%s"
+                "CEF:0|PagoPa|IDPAY|1.0|7|User interaction|2| event=Reward dstip=%s msg=The refund has been calculated suser=%s cs1Label=TRXIssuer cs1=%s cs2Label=TRXAcquirer cs2=%s cs3Label=rewards cs3=%s cs4Label=rejectionReasons cs4=%s cs5Label=initiativeRejectionReasons cs5=%s cs6Label=correlationId cs6=%s"
                         .formatted(
                                 AuditUtilities.SRCIP,
                                 USER,
                                 TRX_ISSUER,
                                 TRX_ACQUIRER,
                                 REWARD,
+                                REJECTIONREASONS,
+                                INIATIATIVEREJECTIONREASONS,
                                 CORRELATION_ID
                                 ),
                 memoryAppender.getLoggedEvents().get(0).getFormattedMessage()
