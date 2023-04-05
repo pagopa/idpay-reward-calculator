63ca27fa93473273ef83874490e59d05d8191bd0
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2023-04-05 17:00:35 +0200

    Discarding corrupted cached container (#118)
    * IDP-619 refactor to store initiative counters on separate documents
    
    * storing reward config
    
    * storing rule builder version
    
    * discarding corrupted cached KieContainer
    
    * testing invalida cached KieContainer

--- src/main/java/it/gov/pagopa/reward/service/reward/RewardContextHolderServiceImpl.java
+++ src/main/java/it/gov/pagopa/reward/service/reward/RewardContextHolderServiceImpl.java
@@ -96,12 +96,18 @@ public class RewardContextHolderServiceImpl implements RewardContextHolderServic
     public void refreshKieContainer(Consumer<? super KieBase> subscriber) {
         if (isRedisCacheEnabled) {
             reactiveRedisTemplate.opsForValue().get(REWARD_CONTEXT_HOLDER_CACHE_NAME)
-                    .map(c -> {
+                    .mapNotNull(c -> {
                         if(!Arrays.equals(c, kieBaseSerialized)){
                             this.kieBaseSerialized = c;
-                            KieBase newKieBase = (KieBase) SerializationUtils.deserialize(c);
-                            preLoadKieBase(newKieBase);
-                            this.kieBase=newKieBase;
+                            try{
+                                KieBase newKieBase = (KieBase) SerializationUtils.deserialize(c);
+                                preLoadKieBase(newKieBase);
+
+                                this.kieBase=newKieBase;
+                            } catch (Exception e){
+                                log.warn("[REWARD_RULE_BUILD] Cached KieContainer cannot be executed! refreshing it!");
+                                return null;
+                            }
                         }
                         return this.kieBase;
                     })
