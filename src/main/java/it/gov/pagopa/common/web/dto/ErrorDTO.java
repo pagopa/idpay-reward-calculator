c36f96619c6575a3def6e47f39dc3c451ad902c1
marco-di-lisa <105736613+marco-di-lisa@users.noreply.github.com>
2023-06-08 10:55:55 +0200

    fix: swagger ui (#144)
    * Fix swagger ui
    
    * Fix swagger ui

--- src/main/java/it/gov/pagopa/common/web/dto/ErrorDTO.java
+++ src/main/java/it/gov/pagopa/common/web/dto/ErrorDTO.java
@@ -1,7 +1,6 @@
 package it.gov.pagopa.common.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import io.swagger.annotations.ApiModelProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,10 +15,8 @@ import lombok.NoArgsConstructor;
 public class ErrorDTO {
 
     @NotBlank
-    @ApiModelProperty(required = true, value = "Code of the error message", example = "Code")
     private String code;
     @NotBlank
-    @ApiModelProperty(required = true, value = "Content of the error message", example = "Message")
     private String message;
 
 }
