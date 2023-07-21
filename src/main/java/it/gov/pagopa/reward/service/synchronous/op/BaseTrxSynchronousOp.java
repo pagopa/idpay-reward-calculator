a9ba252b9544452508014bc0e23a8b405144b25a
GiovanaSolorzano <105281122+GiovanaSolorzano@users.noreply.github.com>
2023-07-21 11:36:10 +0200

    feat: Idp 1368 io come ente voglio poter erogare uniniziativa a sconto destinata ai nuclei familiari (#166)
    * IDP-1368 Initializing family unit for discount initiatives
    
    * add interface and class
    
    * IDP-1407 common class
    
    * IDP-1408 initializing retrieve user counter for family unit initiative
    
    * IDP-1408 fix retrieveCounterEntityId
    
    * IDP-1407 initializing retrieve familyId
    
    * fix HpanInitiativeMediatorServiceImpl
    
    * IDP-1407 Refactor HpanInitiativeMediatorServiceImpl and added test
    
    * IDP-1408 fix findById
    
    * IDP-1407 change requested
    
    * IDP-1408 change requested
    
    * IDP-1408 add integrated test
    
    * IDP-1408 change requested
    
    ---------
    
    Co-authored-by: GiovanaSolorzano <gisolorz@emeal.nttdata.com>
    Co-authored-by: Ranaldo Daniele <Daniele.Ranaldo@emea.nttdata.com>
    Co-authored-by: DanieleRanaldo <124155243+DanieleRanaldo@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/reward/service/synchronous/op/BaseTrxSynchronousOp.java
+++ src/main/java/it/gov/pagopa/reward/service/synchronous/op/BaseTrxSynchronousOp.java
@@ -2,17 +2,24 @@ package it.gov.pagopa.reward.service.synchronous.op;
 
 import it.gov.pagopa.reward.connector.repository.TransactionProcessedRepository;
 import it.gov.pagopa.reward.connector.repository.UserInitiativeCountersRepository;
+import it.gov.pagopa.reward.dto.InitiativeConfig;
+import it.gov.pagopa.reward.dto.build.InitiativeGeneralDTO;
 import it.gov.pagopa.reward.dto.mapper.trx.sync.RewardTransaction2SynchronousTransactionResponseDTOMapper;
 import it.gov.pagopa.reward.dto.mapper.trx.sync.TransactionProcessed2SyncTrxResponseDTOMapper;
 import it.gov.pagopa.reward.dto.synchronous.SynchronousTransactionResponseDTO;
+import it.gov.pagopa.reward.dto.trx.Reward;
 import it.gov.pagopa.reward.dto.trx.RewardTransactionDTO;
 import it.gov.pagopa.reward.dto.trx.TransactionDTO;
 import it.gov.pagopa.reward.exception.TransactionSynchronousException;
+import it.gov.pagopa.reward.model.BaseOnboardingInfo;
+import it.gov.pagopa.reward.model.OnboardingInfo;
 import it.gov.pagopa.reward.model.counters.UserInitiativeCounters;
 import it.gov.pagopa.reward.model.counters.UserInitiativeCountersWrapper;
+import it.gov.pagopa.reward.service.reward.RewardContextHolderService;
 import it.gov.pagopa.reward.service.reward.evaluate.InitiativesEvaluatorFacadeService;
 import it.gov.pagopa.reward.service.synchronous.op.recover.HandleSyncCounterUpdatingTrxService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
 import reactor.core.publisher.Mono;
@@ -31,41 +38,44 @@ abstract class BaseTrxSynchronousOp {
     private final HandleSyncCounterUpdatingTrxService handleSyncCounterUpdatingTrxService;
     private final InitiativesEvaluatorFacadeService initiativesEvaluatorFacadeService;
     private final RewardTransaction2SynchronousTransactionResponseDTOMapper mapper;
+    private final RewardContextHolderService rewardContextHolderService;
 
     protected BaseTrxSynchronousOp(
             TransactionProcessedRepository transactionProcessedRepository,
             TransactionProcessed2SyncTrxResponseDTOMapper syncTrxResponseDTOMapper, UserInitiativeCountersRepository userInitiativeCountersRepository,
             HandleSyncCounterUpdatingTrxService handleSyncCounterUpdatingTrxService,
             InitiativesEvaluatorFacadeService initiativesEvaluatorFacadeService,
-            RewardTransaction2SynchronousTransactionResponseDTOMapper mapper) {
+            RewardTransaction2SynchronousTransactionResponseDTOMapper mapper, RewardContextHolderService rewardContextHolderService) {
         this.transactionProcessedRepository = transactionProcessedRepository;
         this.syncTrxResponseDTOMapper = syncTrxResponseDTOMapper;
         this.userInitiativeCountersRepository = userInitiativeCountersRepository;
         this.handleSyncCounterUpdatingTrxService = handleSyncCounterUpdatingTrxService;
         this.initiativesEvaluatorFacadeService = initiativesEvaluatorFacadeService;
         this.mapper = mapper;
+        this.rewardContextHolderService = rewardContextHolderService;
     }
 
     protected Mono<SynchronousTransactionResponseDTO> checkSyncTrxAlreadyProcessed(String trxId, String initiativeId) {
         return transactionProcessedRepository.findById(trxId)
                 .flatMap(trxProcessed -> {
                             SynchronousTransactionResponseDTO response = syncTrxResponseDTOMapper.apply(trxProcessed, initiativeId);
-                            return userInitiativeCountersRepository.findById(UserInitiativeCounters.buildId(trxProcessed.getUserId(), initiativeId))
-                                    .mapNotNull(ctr -> {
-                                        if (CollectionUtils.isEmpty(ctr.getUpdatingTrxId()) || !ctr.getUpdatingTrxId().contains(trxId)) {
-                                            throw new TransactionSynchronousException(HttpStatus.CONFLICT, response);
-                                        } else {
-                                            log.info("[SYNC_TRANSACTION] Actual trx was stuck! recovering it: trxId:{} counterId:{}", trxId, ctr.getId());
-                                            return null;
-                                        }
-                                    });
+                            return retrieveInitiative2OnboardingInfo(trxProcessed.getRewards().get(initiativeId))
+                                    .flatMap(i2o -> userInitiativeCountersRepository.findById(UserInitiativeCounters.buildId(retrieveCounterEntityId(i2o.getFirst(), i2o.getSecond(), trxProcessed.getUserId()), initiativeId))
+                                            .mapNotNull(ctr -> {
+                                                if (CollectionUtils.isEmpty(ctr.getUpdatingTrxId()) || !ctr.getUpdatingTrxId().contains(trxId)) {
+                                                    throw new TransactionSynchronousException(HttpStatus.CONFLICT, response);
+                                                } else {
+                                                    log.info("[SYNC_TRANSACTION] Actual trx was stuck! recovering it: trxId:{} counterId:{}", trxId, ctr.getId());
+                                                    return null;
+                                                }
+                                            }));
                         }
                 );
     }
 
-    protected Mono<SynchronousTransactionResponseDTO> lockCounterAndEvaluate(Mono<?> trxChecks, TransactionDTO trxDTO, String initiativeId) {
+    protected Mono<SynchronousTransactionResponseDTO> lockCounterAndEvaluate(Mono<Pair<InitiativeConfig, OnboardingInfo>> trxChecks, TransactionDTO trxDTO, String initiativeId) {
         return evaluate(trxChecks, trxDTO, initiativeId,
-                x -> userInitiativeCountersRepository.findByIdThrottled(UserInitiativeCounters.buildId(trxDTO.getUserId(), initiativeId), trxDTO.getId())
+                i2o -> userInitiativeCountersRepository.findByIdThrottled(UserInitiativeCounters.buildId(retrieveCounterEntityId(i2o.getFirst(),i2o.getSecond(),trxDTO), initiativeId), trxDTO.getId())
                         .flatMap(counters -> handleSyncCounterUpdatingTrxService.checkUpdatingTrx(trxDTO, counters)),
                 counters -> initiativesEvaluatorFacadeService.evaluateAndUpdateBudget(
                         trxDTO,
@@ -74,14 +84,30 @@ abstract class BaseTrxSynchronousOp {
         );
     }
 
-    protected <T> Mono<SynchronousTransactionResponseDTO> evaluate(Mono<T> trxChecks, TransactionDTO trxDTO, String initiativeId,
-                                                                   Function<T, Mono<UserInitiativeCounters>> counterRetriever,
-                                                                   Function<UserInitiativeCountersWrapper, Mono<RewardTransactionDTO>> evaluator) {
+    protected Mono<SynchronousTransactionResponseDTO> evaluate(Mono<Pair<InitiativeConfig, OnboardingInfo>> trxChecks, TransactionDTO trxDTO, String initiativeId,
+                                                               Function<Pair<InitiativeConfig, OnboardingInfo>, Mono<UserInitiativeCounters>> counterRetriever,
+                                                               Function<UserInitiativeCountersWrapper, Mono<RewardTransactionDTO>> evaluator) {
         return trxChecks
-                .flatMap(counterRetriever)
-                .defaultIfEmpty(new UserInitiativeCounters(trxDTO.getUserId(), initiativeId))
-                .flatMap(userInitiativeCounters -> evaluator.apply(new UserInitiativeCountersWrapper(trxDTO.getUserId(), new HashMap<>(Map.of(initiativeId, userInitiativeCounters)))))
+                .flatMap(i2o -> counterRetriever.apply(i2o)
+                        .defaultIfEmpty(new UserInitiativeCounters(retrieveCounterEntityId(i2o.getFirst(),i2o.getSecond(),trxDTO), initiativeId))
+                )
+                .flatMap(userInitiativeCounters -> evaluator.apply(new UserInitiativeCountersWrapper(userInitiativeCounters.getUserId(), new HashMap<>(Map.of(initiativeId, userInitiativeCounters)))))
                 .map(rewardTransaction -> mapper.apply(trxDTO.getId(), initiativeId, rewardTransaction));
+    }
+
+    protected String retrieveCounterEntityId(InitiativeConfig initiativeConfig, OnboardingInfo onboardingInfo, TransactionDTO trxDTO) {
+
+            return retrieveCounterEntityId(initiativeConfig, onboardingInfo, trxDTO.getUserId());
+    }
+
+    private   String retrieveCounterEntityId(InitiativeConfig initiativeConfig, OnboardingInfo onboardingInfo, String userId) {
+        return InitiativeGeneralDTO.BeneficiaryTypeEnum.NF.equals(initiativeConfig.getBeneficiaryType())
+                ? onboardingInfo.getFamilyId() : userId;
+    }
+
+    protected Mono<Pair<InitiativeConfig, OnboardingInfo>> retrieveInitiative2OnboardingInfo(Reward reward) {
+        return rewardContextHolderService.getInitiativeConfig(reward.getInitiativeId())
+                .map(i -> Pair.of(i,new BaseOnboardingInfo(reward.getInitiativeId(),reward.getFamilyId())));
     }
 
 }
