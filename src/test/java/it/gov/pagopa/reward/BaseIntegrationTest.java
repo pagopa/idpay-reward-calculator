6567b8a4467e1983efe367329a0b09ca25571a69
anttorre <antonio.torre@nttdata.com>
2023-08-02 18:42:25 +0200

    IDP-1574 fix UserInitiativeCounter Locking Logic

--- src/test/java/it/gov/pagopa/reward/BaseIntegrationTest.java
+++ src/test/java/it/gov/pagopa/reward/BaseIntegrationTest.java
@@ -72,7 +72,7 @@ import java.util.regex.Pattern;
                 //region mongodb
                 "logging.level.org.mongodb.driver=WARN",
                 "logging.level.de.flapdoodle.embed.mongo.spring.autoconfigure=WARN",
-                "de.flapdoodle.mongodb.embedded.version=4.0.21",
+                "de.flapdoodle.mongodb.embedded.version=4.2.24",
                 //endregion
         })
 @AutoConfigureDataMongo
