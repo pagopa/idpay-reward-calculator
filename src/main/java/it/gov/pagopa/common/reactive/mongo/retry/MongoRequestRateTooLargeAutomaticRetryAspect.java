8c1208ced76347dda55b00c6395ecae922372f2a
larissaaiala <larissaaiala.silvaleite.st@nttdata.com>
2023-08-07 18:49:53 +0200

    IDP-1582-rendere-configurabile-il-retry-degli-errori-429-di-mongo-anche-per-le-api-per-gli-applicativi-reattivi

---
+++ src/main/java/it/gov/pagopa/common/reactive/mongo/retry/MongoRequestRateTooLargeAutomaticRetryAspect.java
@@ -0,0 +1,106 @@
+package it.gov.pagopa.common.reactive.mongo.retry;
+
+import it.gov.pagopa.common.reactive.web.ReactiveRequestContextHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.context.ContextView;
+
+@Configuration
+@EnableAspectJAutoProxy
+@Aspect
+@Slf4j
+public class MongoRequestRateTooLargeAutomaticRetryAspect {
+
+    private final boolean enabledApi;
+    private final long maxRetryApi;
+    private final long maxMillisElapsedApi;
+    private final boolean enabledBatch;
+    private final long maxRetryBatch;
+    private final long maxMillisElapsedBatch;
+
+    public MongoRequestRateTooLargeAutomaticRetryAspect(
+        @Value("${mongo.request-rate-too-large.api.enabled}") boolean enabledApi,
+        @Value("${mongo.request-rate-too-large.api.max-retry:3}") long maxRetryApi,
+        @Value("${mongo.request-rate-too-large.api.max-millis-elapsed:0}") long maxMillisElapsedApi,
+        @Value("${mongo.request-rate-too-large.batch.enabled}") boolean enabledBatch,
+        @Value("${mongo.request-rate-too-large.batch.max-retry}") long maxRetryBatch,
+        @Value("${mongo.request-rate-too-large.batch.max-millis-elapsed}") long maxMillisElapsedBatch) {
+        this.enabledApi = enabledApi;
+        this.maxRetryApi = maxRetryApi;
+        this.maxMillisElapsedApi = maxMillisElapsedApi;
+        this.enabledBatch = enabledBatch;
+        this.maxRetryBatch = maxRetryBatch;
+        this.maxMillisElapsedBatch = maxMillisElapsedBatch;
+    }
+
+    @Pointcut("within(*..*Repository*)")
+    public void inRepositoryClass() {
+    }
+
+    @Pointcut("execution(public reactor.core.publisher.Mono *(..))")
+    public void returnMono() {
+    }
+
+    @Pointcut("execution(public reactor.core.publisher.Flux *(..))")
+    public void returnFlux() {
+    }
+
+    @Around("inRepositoryClass() && returnMono()")
+    public Object decorateMonoRepositoryMethods(ProceedingJoinPoint pjp) throws Throwable {
+        Mono<?> out = (Mono<?>) pjp.proceed();
+
+        return Mono.deferContextual(ctx -> {
+            if (isNotControllerContext(ctx)) {
+                if(enabledBatch) {
+                    return MongoRequestRateTooLargeRetryer.withRetry(out, maxRetryBatch,
+                        maxMillisElapsedBatch);
+                }else {
+                    return out;
+                }
+            } else {
+                if(enabledApi){
+                    return MongoRequestRateTooLargeRetryer.withRetry(out, maxRetryApi,
+                        maxMillisElapsedApi);
+                }else {
+                    return out;
+                }
+            }
+        });
+    }
+
+    @Around("inRepositoryClass() && returnFlux()")
+    public Object decorateFluxRepositoryMethods(ProceedingJoinPoint pjp) throws Throwable {
+        @SuppressWarnings("unchecked") // only with Flux the compiler return error when using wildcard, so here we are using Object
+        Flux<Object> out = (Flux<Object>) pjp.proceed();
+
+        return Flux.deferContextual(ctx -> {
+            if (isNotControllerContext(ctx)) {
+                if(enabledBatch) {
+                    return MongoRequestRateTooLargeRetryer.withRetry(out, maxRetryBatch,
+                        maxMillisElapsedBatch);
+                }else {
+                    return out;
+                }
+            } else {
+                if(enabledApi){
+                    return MongoRequestRateTooLargeRetryer.withRetry(out, maxRetryApi,
+                        maxMillisElapsedApi);
+                }else {
+                    return out;
+                }
+            }
+        });
+    }
+
+    private static boolean isNotControllerContext(ContextView ctx) {
+        return ctx.getOrEmpty(ReactiveRequestContextHolder.CONTEXT_KEY).isEmpty();
+    }
+}
