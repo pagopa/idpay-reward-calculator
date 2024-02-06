ffe2d331eb1f11a83e025019bca1bd8fb0bf9ca0
DanieleRanaldo <Daniele.Ranaldo@nttdata.com>
2024-02-06 16:46:27 +0100

    [IDP-2278] added etag on header

--- src/main/java/it/gov/pagopa/reward/controller/RewardTrxSynchronousApiController.java
+++ src/main/java/it/gov/pagopa/reward/controller/RewardTrxSynchronousApiController.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.reward.controller;
 
 import it.gov.pagopa.reward.dto.synchronous.SynchronousTransactionRequestDTO;
 import it.gov.pagopa.reward.dto.synchronous.SynchronousTransactionResponseDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -9,8 +10,8 @@ import reactor.core.publisher.Mono;
 public interface RewardTrxSynchronousApiController {
 
     @PostMapping("/initiative/preview/{initiativeId}")
-    Mono<SynchronousTransactionResponseDTO> previewTransaction(@RequestBody SynchronousTransactionRequestDTO trxPreviewRequest,
-                                                               @PathVariable("initiativeId") String initiativeId);
+    Mono<ResponseEntity<SynchronousTransactionResponseDTO>> previewTransaction(@RequestBody SynchronousTransactionRequestDTO trxPreviewRequest,
+                                                                               @PathVariable("initiativeId") String initiativeId);
 
     @PostMapping("/initiative/{initiativeId}")
     Mono<SynchronousTransactionResponseDTO> authorizeTransaction(@RequestBody SynchronousTransactionRequestDTO trxAuthorizeRequest,
--- src/main/java/it/gov/pagopa/reward/controller/RewardTrxSynchronousApiControllerImpl.java
+++ src/main/java/it/gov/pagopa/reward/controller/RewardTrxSynchronousApiControllerImpl.java
@@ -7,6 +7,8 @@ import it.gov.pagopa.reward.exception.custom.InitiativeNotFoundOrNotDiscountExce
 import it.gov.pagopa.reward.exception.custom.TransactioNotFoundException;
 import it.gov.pagopa.reward.service.synchronous.RewardTrxSynchronousApiService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -22,12 +24,13 @@ public class RewardTrxSynchronousApiControllerImpl implements RewardTrxSynchrono
     }
 
     @Override
-    public Mono<SynchronousTransactionResponseDTO> previewTransaction(SynchronousTransactionRequestDTO trxPreviewRequest, String initiativeId) {
+    public Mono<ResponseEntity<SynchronousTransactionResponseDTO>> previewTransaction(SynchronousTransactionRequestDTO trxPreviewRequest, String initiativeId) {
         log.info("[SYNC_PREVIEW_TRANSACTION] The user {} requests preview of a transaction having id {} on initiativeId {}", trxPreviewRequest.getUserId(), trxPreviewRequest.getTransactionId(), initiativeId);
 
         return PerformanceLogger.logTimingFinally("SYNC_PREVIEW_TRANSACTION",
                 rewardTrxSynchronousService.previewTransaction(trxPreviewRequest, initiativeId)
                         .doOnNext(r -> log.info("[SYNC_PREVIEW_TRANSACTION] The preview requested by userId {} of a transaction having id {} on initiativeId {} resulted into status {} and rejections {}", trxPreviewRequest.getUserId(), trxPreviewRequest.getTransactionId(), initiativeId, r.getStatus(), r.getRejectionReasons()))
+                        .map(r -> ResponseEntity.ok().header(HttpHeaders.ETAG, String.valueOf(r.getReward().getCounters().getVersion())).body(r))
                         .switchIfEmpty(Mono.error(new InitiativeNotFoundOrNotDiscountException(String.format(ExceptionMessage.INITIATIVE_NOT_FOUND_OR_NOT_DISCOUNT_MSG, initiativeId)))),
                 trxPreviewRequest.toString());
     }
