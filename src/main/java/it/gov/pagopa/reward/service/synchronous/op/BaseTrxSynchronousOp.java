8d2c8a712071e5c3fb2c1f4adb54ba9c9d53261e
GiovanaSolorzano <105281122+GiovanaSolorzano@users.noreply.github.com>
2024-02-26 12:22:12 +0100

    feat: Idp 2357 restore refund functionality on reward calculator (#263)
    * [IDP-2357] initializing restore refund functionality
    
    * [IDP-2357] test path
    
    * [IDP-2357] fix update userInitiativeCounters and added unit test
    
    * [IDP-2357] suppress warning too many parameters on BaseTrxSynchronousOp constructor
    
    * [IDP-2357] added requested changes
    
    ---------
    
    Co-authored-by: GiovanaSolorzano <giovanaelvira.solorzanochirieleison@nttdata.com>

--- src/main/java/it/gov/pagopa/reward/service/synchronous/op/BaseTrxSynchronousOp.java
+++ src/main/java/it/gov/pagopa/reward/service/synchronous/op/BaseTrxSynchronousOp.java
@@ -6,17 +6,25 @@ import it.gov.pagopa.reward.dto.InitiativeConfig;
 import it.gov.pagopa.reward.dto.build.InitiativeGeneralDTO;
 import it.gov.pagopa.reward.dto.mapper.trx.Transaction2RewardTransactionMapper;
 import it.gov.pagopa.reward.dto.mapper.trx.sync.RewardTransaction2SynchronousTransactionResponseDTOMapper;
+import it.gov.pagopa.reward.dto.mapper.trx.sync.SynchronousTransactionRequestDTOt2TrxDtoOrResponseMapper;
+import it.gov.pagopa.reward.dto.synchronous.SynchronousTransactionRequestDTO;
 import it.gov.pagopa.reward.dto.synchronous.SynchronousTransactionResponseDTO;
 import it.gov.pagopa.reward.dto.trx.Reward;
 import it.gov.pagopa.reward.dto.trx.RewardTransactionDTO;
 import it.gov.pagopa.reward.dto.trx.TransactionDTO;
+import it.gov.pagopa.reward.enums.InitiativeRewardType;
+import it.gov.pagopa.reward.exception.custom.InitiativeNotActiveException;
+import it.gov.pagopa.reward.exception.custom.InitiativeNotFoundOrNotDiscountException;
+import it.gov.pagopa.reward.exception.custom.InitiativeNotInContainerException;
 import it.gov.pagopa.reward.exception.custom.PendingCounterException;
 import it.gov.pagopa.reward.model.OnboardingInfo;
 import it.gov.pagopa.reward.model.counters.UserInitiativeCounters;
 import it.gov.pagopa.reward.model.counters.UserInitiativeCountersWrapper;
+import it.gov.pagopa.reward.service.reward.OnboardedInitiativesService;
 import it.gov.pagopa.reward.service.reward.RewardContextHolderService;
 import it.gov.pagopa.reward.service.reward.evaluate.InitiativesEvaluatorFacadeService;
 import it.gov.pagopa.reward.service.reward.evaluate.UserInitiativeCountersUpdateService;
+import it.gov.pagopa.reward.utils.RewardConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.util.Pair;
 import reactor.core.publisher.Mono;
@@ -35,20 +43,27 @@ abstract class BaseTrxSynchronousOp {
     protected final RewardContextHolderService rewardContextHolderService;
     protected final Transaction2RewardTransactionMapper rewardTransactionMapper;
     protected final UserInitiativeCountersUpdateService userInitiativeCountersUpdateService;
+    private final OnboardedInitiativesService onboardedInitiativesService;
+    protected final SynchronousTransactionRequestDTOt2TrxDtoOrResponseMapper syncTrxRequest2TransactionDtoMapper;
 
+    @SuppressWarnings("squid:S00107") // suppressing too many parameters alert
     protected BaseTrxSynchronousOp(
             UserInitiativeCountersRepository userInitiativeCountersRepository,
             InitiativesEvaluatorFacadeService initiativesEvaluatorFacadeService,
             RewardTransaction2SynchronousTransactionResponseDTOMapper mapper,
             RewardContextHolderService rewardContextHolderService,
             Transaction2RewardTransactionMapper rewardTransactionMapper,
-            UserInitiativeCountersUpdateService userInitiativeCountersUpdateService) {
+            UserInitiativeCountersUpdateService userInitiativeCountersUpdateService,
+            OnboardedInitiativesService onboardedInitiativesService,
+            SynchronousTransactionRequestDTOt2TrxDtoOrResponseMapper syncTrxRequest2TransactionDtoMapper) {
         this.userInitiativeCountersRepository = userInitiativeCountersRepository;
         this.initiativesEvaluatorFacadeService = initiativesEvaluatorFacadeService;
         this.mapper = mapper;
         this.rewardContextHolderService = rewardContextHolderService;
         this.rewardTransactionMapper = rewardTransactionMapper;
         this.userInitiativeCountersUpdateService = userInitiativeCountersUpdateService;
+        this.onboardedInitiativesService = onboardedInitiativesService;
+        this.syncTrxRequest2TransactionDtoMapper = syncTrxRequest2TransactionDtoMapper;
     }
 
     protected Mono<SynchronousTransactionResponseDTO> evaluate(Mono<Pair<InitiativeConfig, OnboardingInfo>> trxChecks, TransactionDTO trxDTO, String initiativeId,
@@ -111,6 +126,38 @@ abstract class BaseTrxSynchronousOp {
                                     .map(x -> ctr2reward.getSecond());
                         }
                 );
+    }
+
+    protected Mono<UserInitiativeCounters> findUserInitiativeCounter(String initiativeId, Pair<InitiativeConfig, OnboardingInfo> i2o, TransactionDTO trxDTO) {
+        return userInitiativeCountersRepository.findById(UserInitiativeCounters.buildId(retrieveCounterEntityId(i2o.getFirst(), i2o.getSecond(), trxDTO), initiativeId));
+    }
+
+    protected Mono<Boolean> checkInitiative(SynchronousTransactionRequestDTO request, String initiativeId) {
+        return rewardContextHolderService.getInitiativeConfig(initiativeId)
+                .map(i -> InitiativeRewardType.DISCOUNT.equals(i.getInitiativeRewardType()))
+                .switchIfEmpty(Mono.just(false))
+                .map(b -> checkingResult(b, request, initiativeId, RewardConstants.TRX_REJECTION_REASON_INITIATIVE_NOT_FOUND))
+                .map(b -> checkingResult(rewardContextHolderService.getRewardRulesKieInitiativeIds().contains(initiativeId), request, initiativeId, RewardConstants.TRX_REJECTION_REASON_RULE_ENGINE_NOT_READY));
+    }
+
+    protected Mono<Pair<InitiativeConfig, OnboardingInfo>> checkOnboarded(SynchronousTransactionRequestDTO request, TransactionDTO trx, String initiativeId) {
+        return onboardedInitiativesService.isOnboarded(trx.getHpan(), trx.getTrxChargeDate(), initiativeId)
+                .switchIfEmpty(Mono.error(new InitiativeNotActiveException(String.format(RewardConstants.ExceptionMessage.INITIATIVE_NOT_ACTIVE_FOR_USER_MSG,initiativeId),syncTrxRequest2TransactionDtoMapper
+                        .apply(request, initiativeId, List.of(RewardConstants.TRX_REJECTION_REASON_NO_INITIATIVE)))));
+    }
+
+    protected Boolean checkingResult(Boolean b, SynchronousTransactionRequestDTO request, String initiativeId, String trxRejectionReasonNoInitiative) {
+        if (b.equals(Boolean.TRUE)) {
+            return Boolean.TRUE;
+        } else {
+            if (RewardConstants.TRX_REJECTION_REASON_INITIATIVE_NOT_FOUND.equalsIgnoreCase(trxRejectionReasonNoInitiative)) {
+                throw new InitiativeNotFoundOrNotDiscountException(String.format(RewardConstants.ExceptionMessage.INITIATIVE_NOT_FOUND_OR_NOT_DISCOUNT_MSG, initiativeId), syncTrxRequest2TransactionDtoMapper
+                        .apply(request, initiativeId, List.of(trxRejectionReasonNoInitiative)));
+            } else {
+                throw new InitiativeNotInContainerException(String.format(RewardConstants.ExceptionMessage.INITIATIVE_NOT_READY_MSG, initiativeId), syncTrxRequest2TransactionDtoMapper
+                        .apply(request, initiativeId, List.of(trxRejectionReasonNoInitiative)));
+            }
+        }
     }
 
 }
