8e73fbb8e7eb90f281f1bd15e92fefb161d7b42c
and-mora <andrea.morabito@pagopa.it>
2025-10-23 21:02:20 +0200

    add bean definition

--- src/main/java/it/gov/pagopa/common/reactive/mongo/config/ReactiveMongoConfig.java
+++ src/main/java/it/gov/pagopa/common/reactive/mongo/config/ReactiveMongoConfig.java
@@ -1,6 +1,11 @@
 package it.gov.pagopa.common.reactive.mongo.config;
 
+import com.mongodb.reactivestreams.client.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @EnableReactiveMongoRepositories(
@@ -9,5 +14,14 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 )
 @Configuration
 public class ReactiveMongoConfig {
+
+  @Bean(name = "reactiveMongoTemplate")
+  public ReactiveMongoTemplate reactiveMongoTemplate(
+      MongoClient mongoClient,
+      @Value("${spring.data.mongodb.database}") String database,
+      MongoCustomConversions conversions
+  ) {
+    return new ReactiveMongoTemplate(mongoClient, database);
+  }
 
 }
