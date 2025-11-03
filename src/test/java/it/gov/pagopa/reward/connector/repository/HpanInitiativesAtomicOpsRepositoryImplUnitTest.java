2be851d2a86357d90d335b5bb03d477b810822fc
dariopelliccioli <dario.pelliccioli@nttdata.com>
2025-11-03 20:02:45 +0100

    [UPBE-340] feat: Altered temporary tests

--- src/test/java/it/gov/pagopa/reward/connector/repository/HpanInitiativesAtomicOpsRepositoryImplUnitTest.java
+++ src/test/java/it/gov/pagopa/reward/connector/repository/HpanInitiativesAtomicOpsRepositoryImplUnitTest.java
@@ -1,4 +1,43 @@
 package it.gov.pagopa.reward.connector.repository;
 
+import it.gov.pagopa.reward.connector.repository.secondary.HpanInitiativesAtomicOpsRepository;
+import it.gov.pagopa.reward.connector.repository.secondary.HpanInitiativesAtomicOpsRepositoryImpl;
+import it.gov.pagopa.reward.enums.HpanInitiativeStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Temporary Class, to fix integrated tests
+ */
+@ExtendWith(MockitoExtension.class)
+@Slf4j
 public class HpanInitiativesAtomicOpsRepositoryImplUnitTest {
+
+    @Mock
+    private ReactiveMongoTemplate mongoTemplate;
+
+    private HpanInitiativesAtomicOpsRepository hpanInitiativesAtomicOpsRepository;
+
+    @BeforeEach
+    public void init() {
+        hpanInitiativesAtomicOpsRepository = new HpanInitiativesAtomicOpsRepositoryImpl(mongoTemplate);
+    }
+
+    @Test
+    public void testUpdateInitativeStatusOK() {
+        hpanInitiativesAtomicOpsRepository.setUserInitiativeStatus(
+                "USERID","INITIATIVEID", HpanInitiativeStatus.INACTIVE);
+        verify(mongoTemplate).findAndModify(any(),any(),any());
+    }
+
+
 }
